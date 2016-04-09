52b14c19dcbbe70fa1463bd694a8de45fd7ae049
cosine <qjatn1780@naver.com>
2016-04-10 02:35:34 +0900

    modify following pycharm.

--- ropgadget/__init__.py
+++ ropgadget/__init__.py
@@ -1,14 +1,14 @@
-## -*- coding: utf-8 -*-
-##
-##  Jonathan Salwan - 2014-05-12 - ROPgadget tool
-##
-##  http://twitter.com/JonathanSalwan
-##  http://shell-storm.org/project/ROPgadget/
-##
-##  This program is free software: you can redistribute it and/or modify
-##  it under the terms of the GNU General Public License as published by
-##  the Free Software  Foundation, either  version 3 of  the License, or
-##  (at your option) any later version.
+# -*- coding: utf-8 -*-
+#
+#  Jonathan Salwan - 2014-05-12 - ROPgadget tool
+#
+#  http://twitter.com/JonathanSalwan
+#  http://shell-storm.org/project/ROPgadget/
+#
+# This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+# the Free Software  Foundation, either  version 3 of  the License, or
+#  (at your option) any later version.
 
 import ropgadget.args
 import ropgadget.binary
@@ -21,8 +21,9 @@ import ropgadget.version
 import ropgadget.loaders
 import ropgadget.ropchain
 
+
 def main():
     import sys
-    from   ropgadget.args import Args
-    from   ropgadget.core import Core
+    from ropgadget.args import Args
+    from ropgadget.core import Core
     sys.exit(Core(Args().getArgs()).analyze())
--- ropgadget/rgutils.py
+++ ropgadget/rgutils.py
@@ -1,14 +1,15 @@
-## -*- coding: utf-8 -*-
-##
-##  Jonathan Salwan - 2014-05-17 - ROPgadget tool
-## 
-##  http://twitter.com/JonathanSalwan
-##  http://shell-storm.org/project/ROPgadget/
-## 
-##  This program is free software: you can redistribute it and/or modify
-##  it under the terms of the GNU General Public License as published by
-##  the Free Software  Foundation, either  version 3 of  the License, or
-##  (at your option) any later version.
+# -*- coding: utf-8 -*-
+#
+#  Jonathan Salwan - 2014-05-17 - ROPgadget tool
+#
+#  http://twitter.com/JonathanSalwan
+#  http://shell-storm.org/project/ROPgadget/
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software  Foundation, either  version 3 of  the License, or
+#  (at your option) any later version.
+
 
 def deleteDuplicateGadgets(currentGadgets):
     gadgets_content_set = set()
@@ -21,6 +22,6 @@ def deleteDuplicateGadgets(currentGadgets):
         unique_gadgets += [gadget]
     return unique_gadgets
 
-def alphaSortgadgets(currentGadgets):
-    return sorted(currentGadgets, key=lambda key : key["gadget"]) 
 
+def alphaSortgadgets(currentGadgets):
+    return sorted(currentGadgets, key=lambda key: key["gadget"])
